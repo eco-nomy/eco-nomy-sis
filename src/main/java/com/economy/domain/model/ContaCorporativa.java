1e9f70547f257f8e63910b53c0ff31ae7d9b704a
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 21:32:35 -0300

    feat(ContaCorporativa.java): parametros da classe criados

--- src/main/java/com/economy/domain/model/ContaCorporativa.java
+++ src/main/java/com/economy/domain/model/ContaCorporativa.java
@@ -1,1 +1,12 @@
 package com.economy.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+public class ContaCorporativa extends PanacheEntity {
+    public String nome;
+    public BigDecimal balancaDisponivel = BigDecimal.ZERO;
+    public BigDecimal reservado = BigDecimal.ZERO;
+}
