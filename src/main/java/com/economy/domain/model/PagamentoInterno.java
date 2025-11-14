f3defc37cb968492fe8a3e13c391f9d63d1ed5d4
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 21:39:40 -0300

    feat(PagamentoInterno): parametros adicionados

--- src/main/java/com/economy/domain/model/PagamentoInterno.java
+++ src/main/java/com/economy/domain/model/PagamentoInterno.java
@@ -1,1 +1,15 @@
 package com.economy.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class PagamentoInterno extends PanacheEntity {
+    public Long corporacaoId;
+    public Long userId;
+    public BigDecimal quantidade;
+    public String status; // PENDING, COMPLETED, FAILED
+    public LocalDateTime dataCriacao = LocalDateTime.now();
+}
