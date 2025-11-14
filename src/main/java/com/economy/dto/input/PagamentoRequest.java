8d4b3b260b0df8b5e405f7b3f5cf40180b443f4f
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 22:11:18 -0300

    feat(PagamentoRequest): dto de input de pagamento criado

---
+++ src/main/java/com/economy/dto/input/PagamentoRequest.java
@@ -0,0 +1,9 @@
+package com.economy.dto;
+
+import java.math.BigDecimal;
+
+public class PagamentoRequest {
+    public Long origemCorporationId;
+    public Long destinoUserId;
+    public BigDecimal quantia;
+}
