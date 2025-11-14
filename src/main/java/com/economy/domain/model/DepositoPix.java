dd15622302b91b466f34b53397ad527ec14ae64b
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 21:44:45 -0300

    feat(DepositoPix.java): criado parametros

--- src/main/java/com/economy/domain/model/DepositoPix.java
+++ src/main/java/com/economy/domain/model/DepositoPix.java
@@ -1,1 +1,15 @@
 package com.economy.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class DepositoPix extends PanacheEntity {
+    public Long corporationId;
+    public BigDecimal quantia;
+    public String status; // PENDING, CONFIRMED
+    public String mpPagamentoId; // marketplace/gateway id
+    public LocalDateTime dataCriacao = LocalDateTime.now();
+}
