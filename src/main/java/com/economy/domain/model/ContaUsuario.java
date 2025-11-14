0a1e03ecd5ceb76779757f2f1089fb59fc8a96b5
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 21:37:25 -0300

    feat(ContaUsuario.java):parametros adicionados

--- src/main/java/com/economy/domain/model/ContaUsuario.java
+++ src/main/java/com/economy/domain/model/ContaUsuario.java
@@ -1,1 +1,12 @@
-package com.economy.domain.model;
+pckage com.economy.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+public class ContaUsuario extends PanacheEntity {
+  public String nome;
+  public String chavePix;
+  public BigDecimal balance = BigDecimal.ZERO;
+}
