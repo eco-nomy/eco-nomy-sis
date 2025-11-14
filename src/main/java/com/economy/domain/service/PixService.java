c4a39e4c7c97ffd385f65b5c00787aba9eb17fdb
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 23:34:53 -0300

    feat(PixService): metodos adicionados a interface

--- src/main/java/com/economy/domain/service/PixService.java
+++ src/main/java/com/economy/domain/service/PixService.java
@@ -1,4 +1,10 @@
 package com.economy.domain.service;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public interface PixService {
+    public Map<String, Object> createPixQRCode(Long corpId, BigDecimal amount);
+    public Map<String, Object> sendPixPayout(String pixKey, BigDecimal amount);
+    public PixWithdrawal createWithdrawalRecord(Long userId, String pixKey, BigDecimal amount);
 }
