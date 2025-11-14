fd28d22e456e65da89677b48ebebdb86b4947354
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-13 21:47:51 -0300

    feat(DepositoPix): parametros adicionados

--- src/main/java/com/economy/domain/model/SaldarPix.java
+++ src/main/java/com/economy/domain/model/SaldarPix.java
@@ -1,1 +1,16 @@
 package com.economy.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class PixWithdrawal extends PanacheEntity {
+    public Long userId;
+    public BigDecimal quantidade;
+    public String chavePix;
+    public String status; // PENDING, SENT, FAILED
+    public String mpOperationId;
+    public LocalDateTime dataCriacao = LocalDateTime.now();
+}
