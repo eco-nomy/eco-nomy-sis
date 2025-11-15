bcb5311b4b0e43c6f8b7758f67fe51295847eb5d
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-14 21:25:49 -0300

    feat(MercadoPagoClient): Criados metodos de pagamento e payout

--- src/main/java/com/economy/domain/service/MercadoPagoClient.java
+++ src/main/java/com/economy/domain/service/MercadoPagoClient.java
@@ -1,4 +1,27 @@
 package com.economy.domain.service;
 
-public class MercadoPagoClient {
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.POST;
+
+import java.util.Map;
+
+@RegisterRestClient(configKey = "mercadopago-api")
+public interface MercadoPagoClient {
+
+        @POST
+        @Path("/v1/pagamentos")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        Map<String, Object> createPagamento(Map<String, Object> payload);
+
+        @POST
+        @Path("/v1/payouts")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        Map<String, Object> createPayout(Map<String, Object> payload);
+
 }
